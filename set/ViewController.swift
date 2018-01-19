7378aa45bcb6ae579eeab5bf20ab0ae6e71c6689
Patrick Kalkman <patrick@simpletechture.nl>
2018-01-19 13:17:16 +0100

    Added play field with selectable cards

--- set/ViewController.swift
+++ set/ViewController.swift
@@ -10,9 +10,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var setCardButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for buttonIndex in setCardButtons.indices {
+            let button = setCardButtons[buttonIndex]
+            button.layer.borderWidth = 2.0
+            button.layer.borderColor = #colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)
+            button.layer.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+            button.layer.cornerRadius = 10
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func selectCard(_ sender: UIButton) {
+        if (sender.layer.borderWidth == 2.0) {
+            // Select card
+            sender.layer.borderWidth = 4.0
+            sender.layer.borderColor = #colorLiteral(red: 0.5704585314, green: 0.5704723597, blue: 0.5704649091, alpha: 1)
+            sender.layer.backgroundColor = #colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)
+        } else {
+            // Deselect card
+            sender.layer.borderWidth = 2.0
+            sender.layer.borderColor = #colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)
+            sender.layer.backgroundColor = #colorLiteral(red: 0.921431005, green: 0.9214526415, blue: 0.9214410186, alpha: 1)
+        }
+    }
 }
 
